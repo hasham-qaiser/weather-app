ab2a31883fdb244eda81cd13e90f4b4b8c28ecae
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2022-12-01 21:12:11 -0800

    updated html, css & js
    Searchable weather app built with HTML, CSS and JS

---
+++ script.js
@@ -0,0 +1,23 @@
+let weather = {
+  apikey: "15b447c316001d62706bb54dfa03fbbc",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apikey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+  },
+};
